718777a1928c741a43ccbe934c0b2d8cfe3524f2
hieugunny <hieult420@uef.edu.vn>
2023-04-03 22:45:26 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _205051924lLab8.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/SchoolViewModels/EnrollmentDateGroup.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _205051924lLab8.Models.SchoolViewModels
+{
+    public class EnrollmentDateGroup
+    { 
+            [DataType(DataType.Date)]
+            public DateTime? EnrollmentDate { get; set; }
+            public int StudentCount { get; set; } 
+    }
+} 
